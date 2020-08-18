58d339c4db0378393fd425c8dd2ff6e3151dfa2d
pwrstudio <cell@pwr.site>
2020-08-18 09:43:41 +0200

    Basic authentication

--- GameRoom.ts
+++ GameRoom.ts
@@ -1,13 +1,20 @@
 import { Room, Client } from "colyseus";
 import { Schema, MapSchema, ArraySchema, type } from "@colyseus/schema";
 import EasyStar from 'easystarjs'
+import crypto from 'crypto'
+import querystring from 'querystring'
 import fs from 'fs';
 import get from 'lodash/get'
+import * as Sentry from '@sentry/node';
 
+const SSO_SECRET = "daymoon";
+
+// try { 
 const rawdata = fs.readFileSync('hkw-map-color-hard.json');
-let mapMatrix = JSON.parse(rawdata.toString()).data;
-
-// console.dir(mapMatrix)
+const mapMatrix = JSON.parse(rawdata.toString()).data;
+// } catch (err) {
+//   Sentry.captureException(err);
+// }
 
 // 0 = white
 // 1 = black
@@ -16,266 +23,259 @@ let mapMatrix = JSON.parse(rawdata.toString()).data;
 // 4 = green
 // 5 = blue
 
-let easystar = new EasyStar.js();
+const easystar = new EasyStar.js();
 easystar.setGrid(mapMatrix)
 easystar.setAcceptableTiles([0, 2, 3, 4, 5]);
-// easystar.setTileCost(1, 10000);
 // easystar.setIterationsPerCalculation(1000);
 easystar.enableDiagonals();
-easystar.disableCornerCutting()
-
-// var finder = new PF.AStarFinder();
-
-// fs.readFile('hkw-map-array.json', (err, data) => {
-//   if (err) throw err;
-//   let student = JSON.parse(data);
-//   console.log(student);
-// });
+// easystar.disableCornerCutting()
 
 class IP extends Schema {
-  @type("string") address: string;
+    @type("string") address: string;
 }
 
 class Waypoint extends Schema {
-  @type("number") x: number;
-  @type("number") y: number;
+    @type("number") x: number;
+    @type("number") y: number;
 }
 
 class Path extends Schema {
-  @type([Waypoint]) waypoints = new ArraySchema<Waypoint>();
+    @type([Waypoint]) waypoints = new ArraySchema<Waypoint>();
 }
 
 class Player extends Schema {
-  @type("boolean") moderator: boolean;
-  @type("string") uuid: string;
-  @type("string") name: string;
-  @type("string") tint: string;
-  @type("string") ip: string;
-  @type("number") avatar: number;
-  @type("boolean") connected: boolean;
-  @type("number") x: number;
-  @type("number") y: number;
-  @type("number") area: number;
-  @type(Path) path: Path = new Path();
+    @type("boolean") moderator: boolean;
+    @type("string") uuid: string;
+    @type("string") name: string;
+    @type("string") tint: string;
+    @type("string") ip: string;
+    @type("number") avatar: number;
+    @type("boolean") connected: boolean;
+    @type("number") x: number;
+    @type("number") y: number;
+    @type("number") area: number;
+    @type("boolean") authenticated: boolean;
+    @type(Path) path: Path = new Path();
 }
 
 class State extends Schema {
-  @type([IP]) blacklist = new ArraySchema<IP>();
-  @type({ map: Player }) players = new MapSchema();
+    @type([IP]) blacklist = new ArraySchema<IP>();
+    @type({ map: Player }) players = new MapSchema();
 }
 
 export class GameRoom extends Room {
 
+    onCreate(options: any) {
 
-  onCreate(options: any) {
+        this.setState(new State());
 
-    this.setState(new State());
-
-    // Jimp.read('hkw-bit-small.bmp')
-    //   .then(map => {
-    //     // console.dir(map.bitmap.data)
-
-    //     const mapArray = [...map.bitmap.data]
-
-    //     // let firstDimension = 0
-    //     // let secondDimension = 0
-
-    //     let resultArray = []
-
-    //     for (let i = 0; i < 250000; i += 4) {
-    //       resultArray.push(mapArray[i] === 255 ? 1 : 0)
-    //     }
-
-    //     console.dir(resultArray)
-
-    //   })
-    //   .catch(err => {
-    //     console.error(err);
-    //   });
-
-    // this.onMessage("move", (client, message) => {
-    //   // console.log(client.sessionId)
-    //   // console.dir(message.direction)
-    //   if (message.direction === 'up') {
-    //     this.state.players[client.sessionId].y -= 5
-    //   } else if (message.direction === 'down') {
-    //     this.state.players[client.sessionId].y += 5
-    //   } else if (message.direction === 'left') {
-    //     this.state.players[client.sessionId].x -= 5
-    //   } else if (message.direction === 'right') {
-    //     this.state.players[client.sessionId].x += 5
-    //   }
-    // });
-
-    this.onMessage("blacklist", (client, payload) => {
-      console.dir(payload)
-      if (!this.state.blacklist.find((ip: IP) => ip.address == payload.address)) {
-        let newIP = new IP()
-        newIP.address = payload.address
-        this.state.blacklist.push(newIP);
-        for (let key in this.state.players) {
-          if (this.state.players[key].ip == newIP.address) {
-            let bannedClient = this.clients.find((c: Client) => c.id === key)
-            if (bannedClient) {
-              console.log('BANNED:', bannedClient.id)
-              bannedClient.send("banned");
-              bannedClient.leave()
+        this.onMessage("blacklist", (client, payload) => {
+            try {
+                if (!this.state.blacklist.find((ip: IP) => ip.address == payload.address)) {
+                    let newIP = new IP()
+                    newIP.address = payload.address
+                    this.state.blacklist.push(newIP);
+                    for (let key in this.state.players) {
+                        if (this.state.players[key].ip == newIP.address) {
+                            let bannedClient = this.clients.find((c: Client) => c.id === key)
+                            if (bannedClient) {
+                                console.log('BANNED:', bannedClient.id)
+                                bannedClient.send("banned");
+                                bannedClient.leave()
+                            }
+                            delete this.state.players[key]
+                        }
+                    }
+                }
+            } catch (err) {
+                Sentry.captureException(err);
             }
-            delete this.state.players[key]
-          }
-        }
-      }
-    });
+        });
 
-    this.onMessage("whitelist", (client, payload) => {
-      console.dir(payload)
-      let newIP = new IP()
-      newIP.address = payload.address
-      const itemIndex = this.state.blacklist.findIndex((ip: IP) => ip === newIP);
-      console.log(itemIndex)
-      this.state.blacklist.splice(itemIndex, 1);
-    });
+        this.onMessage("whitelist", (client, payload) => {
+            try {
+                let newIP = new IP()
+                newIP.address = payload.address
+                const itemIndex = this.state.blacklist.findIndex((ip: IP) => ip === newIP);
+                this.state.blacklist.splice(itemIndex, 1);
+            } catch (err) {
+                Sentry.captureException(err);
+            }
+        });
 
-    this.onMessage("go", (client, message) => {
-      let roundedX = Math.ceil(get(message, 'x', 0) / 10) * 10
-      let roundedY = Math.ceil(get(message, 'y', 0) / 10) * 10
+        this.onMessage("go", (client, message) => {
+            try {
+                let roundedX = Math.ceil(get(message, 'x', 0) / 10) * 10
+                let roundedY = Math.ceil(get(message, 'y', 0) / 10) * 10
 
-      if (roundedX > 5000) roundedX = 5000
-      if (roundedX < 0) roundedX = 0
+                if (roundedX > 5000) roundedX = 4990
+                if (roundedX < 0) roundedX = 0
 
-      if (roundedY > 5000) roundedY = 5000
-      if (roundedY < 0) roundedY = 0
+                if (roundedY > 5000) roundedY = 4990
+                if (roundedY < 0) roundedY = 0
 
-      console.log('Y', roundedY)
-      console.log('X', roundedX)
-      // console.log('[Y][X] => mapMatrix[' + roundedY / 10 + '][' + roundedX / 10 + '] => ' + mapMatrix[roundedY / 10][roundedX / 10])
-      console.log('- - - - - ')
+                console.log('Y', roundedY)
+                console.log('X', roundedX)
+                console.log('- - - - - ')
 
-      if (mapMatrix[roundedY / 10][roundedX / 10] !== 1) {
+                if (mapMatrix[roundedY / 10][roundedX / 10] !== 1) {
 
-        // let grid = new PF.Grid(mapMatrix);
+                    easystar.findPath(this.state.players[client.sessionId].x / 10,
+                        this.state.players[client.sessionId].y / 10,
+                        roundedX / 10,
+                        roundedY / 10, path => {
+                            if (path === null) {
+                                console.error('no path')
+                            } else {
+                                this.state.players[client.sessionId].x = roundedX;
+                                this.state.players[client.sessionId].y = roundedY;
+                                this.state.players[client.sessionId].path = new Path();
+                                path.forEach(wp => {
+                                    let tempWp = new Waypoint()
+                                    tempWp.y = wp.y * 10
+                                    tempWp.x = wp.x * 10
+                                    this.state.players[client.sessionId].path.waypoints.push(tempWp)
+                                })
+                                let lastWaypoint = path.slice(-1)[0]
+                                this.state.players[client.sessionId].area = mapMatrix[get(lastWaypoint, 'y', 0)][get(lastWaypoint, 'x', 0)]
+                            }
+                        });
 
-        // console.dir(grid)
+                    easystar.calculate();
 
-        // let path = finder.findPath(this.state.players[client.sessionId].y / 10,
-        //   this.state.players[client.sessionId].x / 10,
-        //   roundedY / 10,
-        //   roundedX / 10,
-        //   grid);
+                } else {
+                    // TODO: find closes allowed position
+                    client.send('illegalMove', {})
+                }
+            } catch (err) {
+                Sentry.captureException(err);
+            }
 
-        // console.dir(path)
-        // grid = new PF.Grid(mapMatrix);
-        // let smoothPath = PF.Util.smoothenPath(grid, path);
-        // console.dir(smoothPath)
-        // this.state.players[client.sessionId].x = roundedX;
-        // this.state.players[client.sessionId].y = roundedY;
-        // this.state.players[client.sessionId].path = new Path();
-        // path.forEach(wp => {
-        //   let tempWp = new Waypoint()
-        //   /// !!!!!!!!!! TO FIX
-        //   tempWp.y = wp[0] * 10
-        //   tempWp.x = wp[1] * 10
-        //   this.state.players[client.sessionId].path.waypoints.push(tempWp)
-        // })
+        });
 
+        this.onMessage("teleport", (client, message) => {
+            console.dir(message)
 
-        easystar.findPath(this.state.players[client.sessionId].x / 10,
-          this.state.players[client.sessionId].y / 10,
-          roundedX / 10,
-          roundedY / 10, path => {
-            if (path === null) {
-              console.error('no path')
+            let newX = 0
+            let newY = 0
+
+            let colorIndex = 0
+            if (message.area == 'green')
+                colorIndex = 4
+            else if (message.area == 'blue')
+                colorIndex = 5
+            else if (message.area == 'yellow')
+                colorIndex = 2
+            else if (message.area == 'red')
+                colorIndex = 3
+
+            console.log(colorIndex)
+
+            while (true) {
+                newX = Math.ceil((Math.floor(Math.random() * (4950 - 50 + 1)) + 50) / 10) * 10;
+                newY = Math.ceil((Math.floor(Math.random() * (4950 - 50 + 1)) + 50) / 10) * 10;
+                if (mapMatrix[newY / 10][newX / 10] == colorIndex) break;
+            }
+
+            console.log('TELEPORT')
+            console.log('area', mapMatrix[newY / 10][newX / 10])
+            console.log('=> Y', newY)
+            console.log('=> X', newX)
+            console.log('- - - - - ')
+
+            this.state.players[client.sessionId].area = colorIndex
+            this.state.players[client.sessionId].x = newX
+            this.state.players[client.sessionId].y = newY
+
+        })
+
+    }
+
+    onAuth(client: Client, options: any, request: any) {
+        if (!this.state.blacklist.find((ip: IP) => ip.address == request.connection.remoteAddress)) {
+            console.dir(options)
+
+            options.ip = request.connection.remoteAddress
+
+            if (options.sso && options.sig) {
+
+                console.log('further authenticaion...')
+
+                // Compute the HMAC-SHA256 of sso using sso provider secret as your key.
+                const hmac = crypto.createHmac('sha256', SSO_SECRET);
+                const decoded_sso = decodeURIComponent(options.sso);
+                hmac.update(decoded_sso);
+                const hash = hmac.digest('hex');
+                console.log('hash', hash)
+                if (options.sig == hash) {
+                    const b = Buffer.from(options.sso, 'base64');
+                    const inner_qstring = b.toString('utf8');
+                    const ret = querystring.parse(inner_qstring);
+                    console.dir(ret)
+                    // options.uuid = ret.username
+                    options.name = ret.name || ret.username;
+                    delete options.sso;
+                    delete options.sig;
+                    options.authenticated = true
+                    return (true);
+                }
+                return false
             } else {
-              // console.dir(path)
-              this.state.players[client.sessionId].x = roundedX;
-              this.state.players[client.sessionId].y = roundedY;
-              this.state.players[client.sessionId].path = new Path();
-              path.forEach(wp => {
-                let tempWp = new Waypoint()
-                tempWp.y = wp.y * 10
-                tempWp.x = wp.x * 10
-                this.state.players[client.sessionId].path.waypoints.push(tempWp)
-              })
-              let lastWaypoint = path.slice(-1)[0]
-              this.state.players[client.sessionId].area = mapMatrix[get(lastWaypoint, 'y', 0)][get(lastWaypoint, 'x', 0)]
-              // console.log('AREA:', mapMatrix[path[path.length - 1].y][path[path.length - 1].x])
+                return (true);
             }
-          });
-
-        easystar.calculate();
-
-      } else {
-        // TODO: find closes allowed position 
-      }
-
-    });
-
-  }
-
-  onAuth(client: Client, options: any, request: any) {
-    if (!this.state.blacklist.find((ip: IP) => ip.address == request.connection.remoteAddress)) {
-      return (options.ip = request.connection.remoteAddress);
-    } else {
-      console.log('BANNNEDD')
-      return false
+        } else {
+            console.log('BANNED')
+            return false
+        }
     }
-  }
 
-  onJoin(client: Client, options: any) {
-    console.dir(options);
+    onJoin(client: Client, options: any) {
+        console.dir(options);
 
-    if (!options.moderator) {
+        if (!options.moderator) {
 
-      let startX = 0;
-      let startY = 0;
+            try {
 
-      while (true) {
-        startX = Math.ceil((Math.floor(Math.random() * (4950 - 50 + 1)) + 50) / 10) * 10;
-        startY = Math.ceil((Math.floor(Math.random() * (4950 - 50 + 1)) + 50) / 10) * 10;
-        // Spawn all in green area
-        if (mapMatrix[startY / 10][startX / 10] == 4) break;
-        // if (mapMatrix[startY / 10][startX / 10] !== 1) break;
-      }
+                let startX = 0;
+                let startY = 0;
 
-      this.state.players[client.sessionId] = new Player();
-      this.state.players[client.sessionId].tint = options.tint;
-      this.state.players[client.sessionId].name = options.name;
-      this.state.players[client.sessionId].uuid = options.uuid;
-      this.state.players[client.sessionId].ip = options.ip;
-      this.state.players[client.sessionId].avatar = options.avatar;
-      this.state.players[client.sessionId].connected = true;
-      this.state.players[client.sessionId].x = startX
-      this.state.players[client.sessionId].y = startY
-      this.state.players[client.sessionId].area = mapMatrix[startY / 10][startX / 10]
+                while (true) {
+                    startX = Math.ceil((Math.floor(Math.random() * (4950 - 50 + 1)) + 50) / 10) * 10;
+                    startY = Math.ceil((Math.floor(Math.random() * (4950 - 50 + 1)) + 50) / 10) * 10;
+                    // Spawn all in green area
+                    if (mapMatrix[startY / 10][startX / 10] == 4) break;
+                }
+
+                this.state.players[client.sessionId] = new Player();
+                this.state.players[client.sessionId].authenticated = options.authenticated || false;
+                this.state.players[client.sessionId].tint = options.tint;
+                this.state.players[client.sessionId].name = options.name;
+                this.state.players[client.sessionId].uuid = options.uuid;
+                this.state.players[client.sessionId].ip = options.ip;
+                this.state.players[client.sessionId].avatar = options.avatar;
+                this.state.players[client.sessionId].connected = true;
+                this.state.players[client.sessionId].x = startX
+                this.state.players[client.sessionId].y = startY
+                this.state.players[client.sessionId].area = mapMatrix[startY / 10][startX / 10]
+
+            } catch (err) {
+                Sentry.captureException(err);
+            }
+        }
     }
-  }
 
-  async onLeave(client: Client, consented: boolean) {
-    // // flag client as inactive for other users
-    // this.state.players[client.sessionId].connected = false;
-    console.log('Left:', this.state.players[client.sessionId].name)
-    delete this.state.players[client.sessionId];
+    async onLeave(client: Client, consented: boolean) {
+        try {
+            console.log('Left:', this.state.players[client.sessionId].name)
+            delete this.state.players[client.sessionId];
+        } catch (err) {
+            Sentry.captureException(err);
+        }
+    }
 
-    // try {
-    //   if (consented) {
-    //     throw new Error("consented leave");
-    //   }
-
-
-    //   // allow disconnected client to reconnect into this room until 20 seconds
-    //   await this.allowReconnection(client, 10);
-
-    //   // client returned! let's re-activate it.
-    //   this.state.players[client.sessionId].connected = true;
-
-    // } catch (e) {
-    //   console.log('time expired for:', this.state.players[client.sessionId].name)
-    //   // 20 seconds expired. let's remove the client.
-    //   delete this.state.players[client.sessionId];
-    // }
-  }
-
-  // onDispose() {
-  // }
+    onDispose() {
+        console.log('game room disposed')
+    }
 
 }
+
